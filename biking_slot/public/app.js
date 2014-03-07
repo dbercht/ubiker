dad4462c2a563ea0e1eaef89dc150cc226847114
Daniel Bercht <d.bercht@gmail.com>
2014-03-07 15:20:00 -0500

    Finished initial UI

---
+++ biking_slot/public/app.js
@@ -0,0 +1,100 @@
+/* global angular: false, google: false, console: false, alert: false, window:false */
+angular.module('ubarker', [
+    'ui.map',
+    'ui.event',
+    'ngResource'
+    ])
+.service('Slots', function($resource) {
+  return $resource('/slots/:latitude;:longitude', {latitude:'@latitude', longitude: '@longitude'});
+})
+.controller('UbarkerCtrl', function($scope, Slots, $window) {
+  $scope.slots = [];
+  $scope.loading = false;
+  $scope.myMap = null;
+  $scope.zoom = 13;
+  $scope.mockLat = 37.718951;
+  $scope.mockLong = -122.500339;
+  $scope.userPosition = new google.maps.LatLng(37.7188951, -122.500339);
+  $scope.selectedBikeSlot = null;
+
+  var biciclyingLayer = new google.maps.BicyclingLayer();
+  var directionsService = new google.maps.DirectionsService();
+  var directionsDisplay = new google.maps.DirectionsRenderer();
+
+  $scope.mapOptions = {
+    //zoomControl: false,
+    streetViewControl: false,
+    panControl: false,
+    zoom: $scope.zoom,
+    mapTypeId: google.maps.MapTypeId.RODMAP
+  }; 
+
+  $scope.getLocation = function() {
+    console.log("Getting lcoation");
+    $scope.loading = true;
+    $window.navigator.geolocation.getCurrentPosition(function(position) {
+      $scope.$apply(function() {
+        $scope.userPosition = new google.maps.LatLng($scope.mockLat, $scope.mockLong);
+        //$scope.userPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        $scope.myMap.panTo($scope.userPosition);
+        if ($scope.userMarker !== undefined) {
+          $scope.userMarker.setPosition($scope.userPosition);
+        }
+        $scope.myMarkers = [{'marker' : $scope.userMarker, 'slot' : false} ];
+        $scope.slots = Slots.query({'latitude' : $scope.userPosition.lat(), 'longitude' : $scope.userPosition.lng()}, function(data) {
+          for(var i = 0; i < data.length; i++) {
+            var pos = new google.maps.LatLng(data[i].latitude, data[i].longitude);
+            var obj = {'marker' : new google.maps.Marker({ map: $scope.myMap, position: pos}),'slot': data[i]};
+            $scope.myMarkers.push(obj);
+            //Now select the closest one
+            if (i === 0 && $scope.selectedBikeSlot === null) {
+              $scope.setDestination(obj);
+            }
+          }
+        });
+        $scope.loading = false;
+      });
+    }, function(error) {
+      alert(error);
+    });
+  };
+
+  $scope.$watch('zoom', function() {
+    $scope.myMap.setZoom($scope.zoom);
+  });
+
+  $scope.setDestination = function(obj) {
+    console.log("Changing destination");
+    $scope.selectedBikeSlot = obj;
+    var request = {
+      origin: $scope.userPosition,
+      destination: $scope.selectedBikeSlot.marker.getPosition(),
+      travelMode: google.maps.TravelMode.BICYCLING
+    };
+    directionsService.route(request, function(response, status) {
+      if (status == google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setDirections(response);
+      }
+    });
+  }//;
+
+  //Markers should be added after map is loaded
+  $scope.onMapIdle = function() {
+    if ($scope.myMarkers === undefined){
+      directionsDisplay.setMap($scope.myMap);
+      directionsDisplay.setPanel(document.getElementById("directions-panel"));
+      biciclyingLayer.setMap($scope.myMap);
+      $scope.userMarker = new google.maps.Marker({
+        map: $scope.myMap,
+        position: $scope.userPosition 
+      });
+      $scope.myMarkers = [$scope.userMarker, ];
+    }
+  };
+
+  $scope.markerClicked = function(m) {
+    console.log("Clicked");
+  };
+
+  $scope.getLocation();
+});
